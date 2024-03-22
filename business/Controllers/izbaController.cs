2dcc7a683df7d024bd5eda7ef3e534c2ccc2823d
Izbasarn <146417085+Izbasarn@users.noreply.github.com>
2024-03-22 21:18:35 +0500

    Добавьте файлы проекта.

---
+++ business/Controllers/izbaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace business.Controllers
+{
+    public class izbaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
